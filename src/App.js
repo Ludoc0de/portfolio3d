1ec37950c2625ea144bb589677cd6a8ef786315a
ludoc0de <champi.ludo@tutanota.com>
2022-11-30 16:17:18 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Objects from "./components/Objects"
+import Main from "./components/Main"
 import { Canvas } from "@react-three/fiber"
-
+import { Html } from "@react-three/drei"
 
 export default function App(){
 
     return (
-        <Canvas>
-            <Objects />
+        <Canvas 
+            camera={{ 
+                fov: 70, 
+                near: 0.1, 
+                far: 200,
+                position: [ 0, 0, 120 ]
+            }}
+        >
+            <Main />
+            {/* <Objects /> */}
         </Canvas>
     )
 }
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import { Html } from "@react-three/drei"
+
+export default function Main(){
+     return (
+        <Html>
+            <h1>Ludo code !</h1>
+        </Html>
+     )
+}
