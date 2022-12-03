3cae5a01627a6315bf5cadc3f8774a9d19dd948d
ludoc0de <champi.ludo@tutanota.com>
2022-12-02 20:32:16 -0500

    upload textures images, use gradient in mesh

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -1,21 +1,19 @@
 // import { Canvas } from "@react-three/fiber"
+import texture from '../textures/3.jpg'
 import { useTexture } from "@react-three/drei"
 import { useRef } from "react"
 import * as THREE from 'three'
+import { NearestFilter } from 'three'
 
 export default function Objects(){
 
     const directionalLight = useRef()
-    
     const props = useTexture({ 
-        map: '/static/textures/3.jpg',
-        // displacementMap: '/static/textures/3.jpg',
-        // normalMap: '/static/textures/3.jpg',
-        // roughnessMap: '/static/textures/3.jpg',
-        // aoMap: '/static/textures/3.jpg',
+        map: texture
     })
      
-    // console.log(props)
+    const gradientTexture = props.map
+    gradientTexture.magFilter = THREE.NearestFilter
     
     return (
         <>
@@ -25,19 +23,20 @@ export default function Objects(){
                 color="#ffffff" 
                 intensity={ 1 } 
             />
+
             <mesh>
                 <torusGeometry args={[1, 0.4, 16, 60]} />
-                <meshToonMaterial />
+                <meshToonMaterial gradientMap={gradientTexture}  />
             </mesh>
             
             <mesh>
                 <coneGeometry args={[1, 2, 32]} />
-                <meshToonMaterial />
+                <meshToonMaterial gradientMap={gradientTexture} />
             </mesh>
 
             <mesh>
                 <torusKnotGeometry args={[0.8, 0.35, 100, 16]}/>
-                <meshToonMaterial />
+                <meshToonMaterial gradientMap={gradientTexture}  />
                 {/* <texture attach='map' image={props.map} magFilter={THREE.NearestFilter} /> */}
             </mesh>
 
