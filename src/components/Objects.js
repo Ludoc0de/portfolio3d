d17784d37898df49aeaaa22f80795b6f4c2a89e9
ludoc0de <champi.ludo@tutanota.com>
2022-12-02 19:03:00 -0500

    update

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -1,11 +1,22 @@
 // import { Canvas } from "@react-three/fiber"
-
+import { useTexture } from "@react-three/drei"
 import { useRef } from "react"
+import * as THREE from 'three'
 
 export default function Objects(){
 
     const directionalLight = useRef()
-
+    
+    const props = useTexture({ 
+        map: '/static/textures/3.jpg',
+        // displacementMap: '/static/textures/3.jpg',
+        // normalMap: '/static/textures/3.jpg',
+        // roughnessMap: '/static/textures/3.jpg',
+        // aoMap: '/static/textures/3.jpg',
+    })
+     
+    // console.log(props)
+    
     return (
         <>
             <directionalLight 
@@ -14,14 +25,22 @@ export default function Objects(){
                 color="#ffffff" 
                 intensity={ 1 } 
             />
-            <mesh position-x={1} scale={1}>
-                <boxGeometry />
-                <meshToonMaterial/>
-            </mesh>
             <mesh>
-                <sphereGeometry />
-                <meshBasicMaterial />
+                <torusGeometry args={[1, 0.4, 16, 60]} />
+                <meshToonMaterial />
             </mesh>
+            
+            <mesh>
+                <coneGeometry args={[1, 2, 32]} />
+                <meshToonMaterial />
+            </mesh>
+
+            <mesh>
+                <torusKnotGeometry args={[0.8, 0.35, 100, 16]}/>
+                <meshToonMaterial />
+                {/* <texture attach='map' image={props.map} magFilter={THREE.NearestFilter} /> */}
+            </mesh>
+
         </>
     )
 }
