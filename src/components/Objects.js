1221f3ee6aa261d304a01120353487c92adfcc7a
ludoc0de <champi.ludo@tutanota.com>
2022-12-02 23:20:21 -0500

    animate objects

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -4,6 +4,7 @@ import { useTexture } from "@react-three/drei"
 import { useRef } from "react"
 import * as THREE from 'three'
 import { NearestFilter } from 'three'
+import { useFrame } from '@react-three/fiber'
 
 export default function Objects(){
 
@@ -16,6 +17,20 @@ export default function Objects(){
     gradientTexture.magFilter = THREE.NearestFilter
     
     const objectsDistance = 4
+    
+    const torusRef=useRef()
+    const coneRef=useRef()
+    const toruskRef=useRef()
+
+    const objectsRef = [torusRef, coneRef, toruskRef]
+
+    useFrame((state, delta) =>{
+        for(const object of objectsRef){
+            object.current.rotation.y += delta*0.1
+            object.current.rotation.x += delta*0.15
+        }
+    })
+
 
     return (
         <>
@@ -26,17 +41,17 @@ export default function Objects(){
                 intensity={ 1 } 
             />
 
-            <mesh position-y={[- objectsDistance * 0]}>
+            <mesh ref={torusRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 0]}>
                 <torusGeometry args={[1, 0.4, 16, 60]} />
                 <meshToonMaterial gradientMap={gradientTexture}  />
             </mesh>
             
-            <mesh position-y={[- objectsDistance * 1]}>
+            <mesh ref={coneRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 1]}>
                 <coneGeometry args={[1, 2, 32]} />
                 <meshToonMaterial gradientMap={gradientTexture} />
             </mesh>
 
-            <mesh position-y={[- objectsDistance * 2]}>
+            <mesh ref={toruskRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 2]}>
                 <torusKnotGeometry args={[0.8, 0.35, 100, 16]}/>
                 <meshToonMaterial gradientMap={gradientTexture}  />
             </mesh>
