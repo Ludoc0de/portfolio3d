d32151b22b4f0a19e1c922b92461c8c5d2b0e61c
ludoc0de <champi.ludo@tutanota.com>
2022-12-04 23:06:59 -0500

    clean and update distance y

--- src/components/Objects.js
+++ src/components/Objects.js
@@ -4,7 +4,7 @@ import { useTexture, useScroll } from "@react-three/drei"
 import { useRef } from "react"
 import useRefs from 'react-use-refs'
 import * as THREE from 'three'
-import { NearestFilter } from 'three'
+// import { NearestFilter } from 'three'
 import { useFrame } from '@react-three/fiber'
 
 export default function Objects(){
@@ -17,11 +17,12 @@ export default function Objects(){
     const gradientTexture = propsTexture.map
     gradientTexture.magFilter = THREE.NearestFilter
     
-    const objectsDistance = 4
+    const objectsDistance = 6
 
     const [torusRef, coneRef, toruskRef] = useRefs()
 
     const objectsRef = [torusRef, coneRef, toruskRef]
+
 
     useFrame((state, delta) =>{
         for(const object of objectsRef){
@@ -29,27 +30,6 @@ export default function Objects(){
             object.current.rotation.x += delta*0.15
         }
     })
-
-        function Demo(props){
-            const boxRef = useRef()
-            const data = useScroll()
-            useFrame((state, delta) => {
-                data.offset = 0
-                data.delta= 1
-
-                const a = data.range(0, 1 / 3)
-                const b = data.range(1 / 3, 1 / 3)
-                // const c = data.range(1, 3 / 3)
-            })
-
-            console.log(props)
-            return (
-                <mesh ref={boxRef} {...props}>
-                    <boxGeometry/>
-                    <meshToonMaterial />
-                </mesh>
-            )
-        }
 
     return (
         <>
@@ -59,18 +39,17 @@ export default function Objects(){
                 color="#ffffff" 
                 intensity={ 1 } 
             />
-            <Demo />
             <mesh  ref={torusRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 0]}>
                 <torusGeometry args={[1, 0.4, 16, 60]} />
                 <meshToonMaterial gradientMap={gradientTexture}  />
             </mesh>
             
-            <mesh ref={coneRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 1]}>
+            <mesh ref={coneRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 1.2]}>
                 <coneGeometry args={[1, 2, 32]} />
                 <meshToonMaterial gradientMap={gradientTexture} />
             </mesh>
 
-            <mesh ref={toruskRef} rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 2]}>
+            <mesh ref={toruskRef}    rotation-y={ Math.PI * 0.25 } position-y={[- objectsDistance * 2.6]}>
                 <torusKnotGeometry args={[0.8, 0.35, 100, 16]}/>
                 <meshToonMaterial gradientMap={gradientTexture}  />
             </mesh>
