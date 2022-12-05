eed2db0dc20f81babc03e271202f17172af7772c
ludoc0de <champi.ludo@tutanota.com>
2022-12-04 19:30:03 -0500

    add style

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,13 +3,42 @@ import { Html, Scroll } from "@react-three/drei"
 export default function Main(){
      return (
         <div className="main">
-            <section className="section">
+            <section 
+                style={{
+                    position: 'absolute',
+                    top: '40vh',
+                    // left: '5vw',
+                    // transform: 'translateX(-50%)',
+                    // color: 'green',
+                    marginLeft: '1em',
+                }}
+            >
                 <h1>Ludo code !</h1>
             </section>
-            <section className="section">
+            <section
+                style={{
+                    position: 'absolute',
+                    top: '140vh',
+                    // left: '50vw',
+                    transform: 'translateX(100%)',
+                    // color: '#f4b677',
+                    margin: 0,
+                    marginLeft: '1em',
+                }}
+            >
                 <h1>My Portfolio</h1>
             </section>
-            <section className="section">
+            <section 
+                style={{
+                    position: 'absolute',
+                    top: '250vh',
+                    // left: '50vw',
+                    // transform: 'translateX(-50%)',
+                    // color: '#673ab7',
+                    margin: 0,
+                    marginLeft: '1em',
+                }}
+            >
                 <h1>Contact me</h1>
             </section>
         </div>
