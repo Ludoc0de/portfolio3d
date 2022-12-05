7b1e68122303a04b081d315e5b20eb3b32566def
ludoc0de <champi.ludo@tutanota.com>
2022-12-05 11:56:48 -0500

    uncomment z rotation animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Main from "./components/Main"
 import Objects from "./components/Objects"
+import Particles from "./components/Particles"
 import { Canvas, useFrame } from "@react-three/fiber"
 import { ScrollControls, Scroll } from "@react-three/drei"
 import * as THREE from 'three'
@@ -8,14 +9,15 @@ function ScrollAnimation(){
     useFrame(({mouse, camera}) => {
         camera.position.x = THREE.MathUtils.lerp(camera.position.x, mouse.x * 0.5, 0.03)
         camera.position.y = THREE.MathUtils.lerp(camera.position.y, mouse.y * 0.5, 0.03)
-        //camera.position.z = THREE.MathUtils.lerp(camera.position.z, Math.max(4, Math.abs(mouse.x * mouse.y * 8)), 0.01)
-        //camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, mouse.x * -Math.PI * 0.025, 0.001)
+        // camera.position.z = THREE.MathUtils.lerp(camera.position.z, Math.max(4, Math.abs(mouse.x * mouse.y * 8)), 0.01)
+        camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, mouse.x * -Math.PI * 0.025, 0.005)
     })
 
     return (
         <ScrollControls pages={3}>
             <Scroll>
                 <Objects />
+                <Particles />
             </Scroll>
             <Scroll html>
                 <Main />
