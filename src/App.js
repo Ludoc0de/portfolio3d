83a8bafdade825641a11f740b3f7d649ba217743
ludoc0de <champi.ludo@tutanota.com>
2022-12-05 00:07:21 -0500

    work on scroll parallax

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import Main from "./components/Main"
 import Objects from "./components/Objects"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, useFrame } from "@react-three/fiber"
 import { ScrollControls, Scroll } from "@react-three/drei"
+import * as THREE from 'three'
 
+function ScrollAnimation(){
+    useFrame(({mouse, camera}) => {
+        camera.position.x = THREE.MathUtils.lerp(camera.position.x, mouse.x * 0.5, 0.03)
+    })
+
+    return (
+        <ScrollControls pages={3}>
+            <Scroll>
+                <Objects />
+            </Scroll>
+            <Scroll html>
+                <Main />
+            </Scroll>
+        </ScrollControls>
+    )
+}
 
 export default function App(){
 
     return (
         <>
             <Canvas>
-                <ScrollControls pages={3}>
-                    <Scroll>
-                        <Objects />
-                    </Scroll>
-                    <Scroll html>
-                        <Main />
-                    </Scroll>
-                </ScrollControls>
+                <ScrollAnimation />
             </Canvas>
         </>
     )
