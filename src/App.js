452112d830a24873a0cfa0827196269b2ebd48de
ludoc0de <champi.ludo@tutanota.com>
2022-12-05 11:23:48 -0500

    add y rotation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import * as THREE from 'three'
 function ScrollAnimation(){
     useFrame(({mouse, camera}) => {
         camera.position.x = THREE.MathUtils.lerp(camera.position.x, mouse.x * 0.5, 0.03)
+        camera.position.y = THREE.MathUtils.lerp(camera.position.y, mouse.y * 0.5, 0.03)
+        //camera.position.z = THREE.MathUtils.lerp(camera.position.z, Math.max(4, Math.abs(mouse.x * mouse.y * 8)), 0.01)
+        //camera.rotation.y = THREE.MathUtils.lerp(camera.rotation.y, mouse.x * -Math.PI * 0.025, 0.001)
     })
 
     return (
