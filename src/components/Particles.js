de6bff9e18f8cc4f7445443fcdde8f530cddd5cc
ludoc0de <champi.ludo@tutanota.com>
2022-12-11 15:50:55 -0500

    clean comment

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -5,7 +5,7 @@ import { SphereGeometry, bufferAttribute, bufferGeometry } from 'three'
 import * as THREE from 'three'
 
 export default function Particles({data}){
-    //not working, replace 3 / data
+    //replace 3 / data not working !
     //data = number of mesh from object
     console.log(data)
 
@@ -17,46 +17,14 @@ export default function Particles({data}){
         positions[i*3+0] = (Math.random() - 0.5)* width
         //3 = number of mesh from object
         positions[i*3+1] = 0.5 * height - Math.random()  * height * 3
-        console.log(test)
         positions[i*3+2] = (Math.random() - 0.5)* width
 
     }
-    // const { width, height } = useThree((state) => state.viewport)
-    // console.log( 
-    //     width,
-    //     height,
-    //     Math.random() * width,
-    //     Math.random() * height,
-    // )
 
     return(
         // <Stars  radius={10} depth={50} count={2000} factor={5} saturation={0} fade speed={2} /> 
         //<Sparkles color={'yellow'} count={1000} scale={1} size={1} speed={0.4} />
        
-        // <Points
-        //     limit={particlesCount} 
-        //     // range={10}
-        // >
-        //     <pointsMaterial size={0.04} vertexColors />
-            
-        //     {Array.from({ length: particlesCount }).map((_, i) => (
-        //         <Point
-        //             key={i}
-        //             position={[
-        //                 Math.random() * width,
-        //                 Math.random() * height,
-        //                 Math.random()
-        //                 // (0.5 - Math.random()) * width * 2,
-        //                 // 0.5 * height + Math.random() ** 0.25 * height * -3,
-        //                 // (0.5 - Math.random()) * 25,
-        //             ]}
-        //             color={"red"}
-        //         />
-        //     ))}
-
-        // </Points>
-
-        //exo
         <points>
             <bufferGeometry>
                 <bufferAttribute 
@@ -66,9 +34,7 @@ export default function Particles({data}){
                     array={positions}
                 />
             </bufferGeometry>
-          {/* <sphereGeometry args={[1, 32, 32]} /> */}
-          {/* <meshBasicMaterial color="red"/> */}
-          <pointsMaterial size={0.02} sizeAttenuation={true} color={"red"} />
+            <pointsMaterial size={0.02} sizeAttenuation={true} color={"red"} />
         </points>
     )
 }
