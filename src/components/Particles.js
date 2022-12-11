17eadf92e4f3a74b52d65136fe9dc430eb1eb2df
ludoc0de <champi.ludo@tutanota.com>
2022-12-11 15:39:58 -0500

    clean

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -4,25 +4,19 @@ import { useThree } from '@react-three/fiber'
 import { SphereGeometry, bufferAttribute, bufferGeometry } from 'three'
 import * as THREE from 'three'
 
-export default function Particles({test}){
-    //
-    const [demo, setDemo]=useState()
-    console.log(test)
-    // useEffect(()=>{
-    //     if(test > 0){
-    //         setDemo(test)
-    //     }
-    // }, [test])
-    // console.log(demo)
-    
+export default function Particles({data}){
+    //not working, replace 3 / data
+    //data = number of mesh from object
+    console.log(data)
+
     const particlesCount = 1000
     const positions = new Float32Array(particlesCount * 3)
     const { width, height } = useThree((state) => state.viewport)
   
     for(let i = 0; i < particlesCount; i++){
         positions[i*3+0] = (Math.random() - 0.5)* width
-        //data = number of mesh from object
-        positions[i*3+1] = 0.5 * height - Math.random()  * height * test
+        //3 = number of mesh from object
+        positions[i*3+1] = 0.5 * height - Math.random()  * height * 3
         console.log(test)
         positions[i*3+2] = (Math.random() - 0.5)* width
 
