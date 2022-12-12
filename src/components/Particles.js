3603b70233175287500ee12c31524ecd3beb6dd5
ludoc0de <champi.ludo@tutanota.com>
2022-12-12 09:06:11 -0500

    update add map

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -12,6 +12,10 @@ export default function Particles({data}){
     const particlesCount = 1000
     const positions = new Float32Array(particlesCount * 3)
     //
+    const pointArray= [...Array(particlesCount)]
+    // pointArray.map(()=>{
+    //     console.log('1')
+    // })
     const colors = new Float32Array(particlesCount * 3)
     // console.log(colors[Math.floor(Math.random() * (colors.length - 1))])
     //color={particleColors[Math.floor(Math.random() * (particleColors.length - 1))]}
@@ -32,13 +36,16 @@ export default function Particles({data}){
     return(
         // <Stars  radius={10} depth={50} count={2000} factor={5} saturation={0} fade speed={2} /> 
         //<Sparkles color={'yellow'} count={1000} scale={1} size={1} speed={0.4} />
-        
-        <Points limit={size}>
+
+        <Points limit={particlesCount}>
             <pointsMaterial size={0.05} vertexColors />
-            {Array.from({ length: size }).map((_, i) => (
+            {pointArray.map((value, index) => (
                 <Point
-                key={i}
+                key={index}
                 position={[
+                    (Math.random() - 0.5)* width,
+                    0.5 * height - Math.random()  * height * 3,
+                    (Math.random() - 0.5)* width
                     // (0.5 - Math.random()) * width * 2,
                     // 0.5 * height + Math.random() ** 0.25 * height * -3,
                     // (0.5 - Math.random()) * 25,
