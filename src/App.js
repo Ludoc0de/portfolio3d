beb495feb9b23d99dd51f529a51702f81156905c
ludoc0de <champi.ludo@tutanota.com>
2022-12-13 11:06:22 -0500

    check scrollY

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import ScrollAnimation from "./components/ScrollAnimation"
 import { Canvas } from "@react-three/fiber"
 
 export default function App(){
+    
+    const demo = () => {
+        console.log('ok')
+        console.log(window.scrollY)
+    };
 
     return (
         <>
-            <Canvas>
+            <Canvas onClick={demo}>
                 <ScrollAnimation />
             </Canvas>
         </>
