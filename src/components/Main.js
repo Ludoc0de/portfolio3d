3db699bb50d7d064774b550d1b3a3446de2b4e81
ludoc0de <champi.ludo@tutanota.com>
2022-12-28 00:21:11 -0500

    some font test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,45 +1,45 @@
-
-export default function Main(){ 
-    return (
-        <div className="main">
-            <section 
-                style={{
-                    position: 'absolute',
-                    top: '45vh',
-                    // left: '5vw',
-                    // transform: 'translateX(-50%)',
-                    // color: 'green',
-                    marginLeft: '1em',
-                }}
-            >
-                <h1>Ludo code !</h1>
-            </section>
-            <section
-                style={{
-                    position: 'absolute',
-                    top: '140vh',
-                    // left: '50vw',
-                    transform: 'translateX(100%)',
-                    // color: '#f4b677',
-                    margin: 0,
-                    marginLeft: '1em',
-                }}
-            >
-                <h1>My Portfolio</h1>
-            </section>
-            <section 
-                style={{
-                    position: 'absolute',
-                    top: '250vh',
-                    // left: '50vw',
-                    // transform: 'translateX(-50%)',
-                    // color: '#673ab7',
-                    margin: 0,
-                    marginLeft: '1em',
-                }}
-            >
-                <h1>Contact me</h1>
-            </section>
-        </div>
-    )
+export default function Main() {
+  return (
+    <div className="main">
+      <section
+        style={{
+          position: "absolute",
+          top: "45vh",
+          // left: '5vw',
+          // transform: 'translateX(-50%)',
+          // color: 'green',
+          marginLeft: "1em",
+        }}
+      >
+        <h1>Ludo code !</h1>
+        <p>test gG</p>
+      </section>
+      <section
+        style={{
+          position: "absolute",
+          top: "140vh",
+          // left: '50vw',
+          transform: "translateX(100%)",
+          // color: '#f4b677',
+          margin: 0,
+          marginLeft: "1em",
+        }}
+      >
+        <h1>My Portfolio</h1>
+      </section>
+      <section
+        style={{
+          position: "absolute",
+          top: "250vh",
+          // left: '50vw',
+          // transform: 'translateX(-50%)',
+          // color: '#673ab7',
+          margin: 0,
+          marginLeft: "1em",
+        }}
+      >
+        <h1>Contact me</h1>
+      </section>
+    </div>
+  );
 }
