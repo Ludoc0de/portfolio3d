48f88189165c30743f2a5a676f26701accb736b7
ludoc0de <champi.ludo@tutanota.com>
2023-03-06 11:22:55 -0300

    update ui

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,24 @@
 export default function Main() {
   return (
     <div className="main">
-      <section
-        style={{
-          position: "absolute",
-          // top: "45vh",
-          top: "25vh",
-          marginLeft: "1em",
-        }}
-      >
-        <h1>Ludo code !</h1>
-        <p>Software developpeur</p>
+      <section className="presentation">
+        <h3>Ludo code !</h3>
+        <h1 className="presentation__title">Votre developpeur logiciels</h1>
+        <div className="presentation__text">
+          <p style={{ fontSize: "0.5em" }}>
+            Ludo, développeur web passionné, curieux et dynamique. Expérience en
+            HTML, CSS et JavaScript. Motivé par le challenge de créer vos
+            demandes colorés et personnalisés. Portfolio en ligne disponible.
+          </p>
+        </div>
       </section>
-      <section
-        className="coneRef"
-        style={{
-          position: "absolute",
-          // top: "140vh",
-          top: "120vh",
-          transform: "translateX(100%)",
-          margin: 0,
-          marginLeft: "1em",
-        }}
-      >
-        <h1>My Portfolio</h1>
+      <section className="portfolio">
+        <h1>Mon Portfolio</h1>
+        <h3 className="portfolio__title">Mes derniers projets</h3>
       </section>
+
       <section
+        className="contact"
         style={{
           position: "absolute",
           // top: "250vh",
