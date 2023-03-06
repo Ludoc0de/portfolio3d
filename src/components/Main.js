132085d6611e21cc67fdd4eff3aca7f81f1406c5
ludoc0de <champi.ludo@tutanota.com>
2023-03-06 12:02:21 -0300

    add img

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import heroImage from "../image/template.png";
 export default function Main() {
   return (
     <div className="main">
@@ -5,6 +6,7 @@ export default function Main() {
         <h3>Ludo code !</h3>
         <h1 className="presentation__title">Votre developpeur logiciels</h1>
         <div className="presentation__text">
+          <img className="presentation__logo" src={heroImage} />
           <p style={{ fontSize: "0.5em" }}>
             Ludo, développeur web passionné, curieux et dynamique. Expérience en
             HTML, CSS et JavaScript. Motivé par le challenge de créer vos
@@ -12,21 +14,33 @@ export default function Main() {
           </p>
         </div>
       </section>
+
       <section className="portfolio">
         <h1>Mon Portfolio</h1>
         <h3 className="portfolio__title">Mes derniers projets</h3>
+
+        <div>
+          <a target="_blank" href="https://skypictures.netlify.app/">
+            <div className="container">
+              <img
+                className="section__img_size"
+                src="./images/skypictures.png"
+                alt="photo de l'espace"
+              />
+              <div className="text overlay">cliquer pour visiter</div>
+            </div>
+          </a>
+          <h3>La haut, dans le ciel !</h3>
+          <p>
+            <strong>Javascript OOP, API, SASS, GitHub</strong>
+            <br />
+            Site avec l'api de la Nasa. Création d'une page récupérant les
+            photos du ciel de la Nasa.
+          </p>
+        </div>
       </section>
 
-      <section
-        className="contact"
-        style={{
-          position: "absolute",
-          // top: "250vh",
-          top: "230vh",
-          margin: 0,
-          marginLeft: "1em",
-        }}
-      >
+      <section className="contact">
         <h1>Contact me</h1>
       </section>
     </div>
