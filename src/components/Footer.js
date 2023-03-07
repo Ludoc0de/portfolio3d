28296c30997c2e77bc7826a4a823c8efc064b6a9
ludoc0de <champi.ludo@tutanota.com>
2023-03-07 15:16:43 -0300

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer>
+      <ul class="menu">
+        <li>&copy; Neographe. tous droits réservés.</li>
+        <li></li>
+      </ul>
+    </footer>
+  );
+}
