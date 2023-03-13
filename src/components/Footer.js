322d29005d6d9b3fadb354f621cf667d27897317
ludoc0de <champi.ludo@tutanota.com>
2023-03-13 11:10:35 -0300

    add icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,17 @@
+import { FaTwitter, FaGithub } from "react-icons/fa";
 export default function Footer() {
   return (
-    <footer className="grid grid-rows-1">
-      <ul className="icons">
+    <footer className="grid grid-cols-1">
+      <ul className="grid grid-cols-2 w-20">
         <li>
-          <a
-            href="https://twitter.com/neographe_org"
-            class="icon brands fa-twitter"
-          ></a>
+          <a href="https://twitter.com/neographe_org" className="">
+            <FaTwitter className="pl-0" />
+          </a>
         </li>
         <li>
-          <a
-            href="https://github.com/Asakuracl"
-            class="icon brands fa-github"
-          ></a>
+          <a href="https://github.com/ludocode" className="">
+            <FaGithub className="" />
+          </a>
         </li>
       </ul>
       <ul className="menu text-lg lg:text-xl">
